cdc2fc7c430ae525d618442e6f4838bdce335df9
Marcelo Barbosa <62026017+marceloppb@users.noreply.github.com>
2021-09-01 18:44:20 -0300

    feat: adicionando algumas interfaces globais

---
+++ src/interfaces/TimeLine.interfaces.ts
@@ -0,0 +1,11 @@
+export interface TimelineProps {
+  check_in: boolean,
+  check_in_display: string,
+  id: number,
+  latitude?: string,
+  longitude?: string,
+  minimum_break: boolean,
+  position: number,
+  rectification?: string,
+  worktime_clock: string,
+}
---
+++ src/interfaces/index.ts
@@ -0,0 +1,1 @@
+export { TimelineProps } from './TimeLine.interfaces'
